af4412904151d4c18af43464ba620d1014bbedf7
roberto-mardeni <rmardeni@gmail.com>
2023-07-27 11:08:04 -0400

    Added method

--- c#/sample.cs
+++ c#/sample.cs
@@ -5,6 +5,8 @@ namespace sample
     public static class Program
     {
         public static long calculateDaysBetweenDates(DateTime begin, DateTime end)
+
+        public static long calculateMinutesBetweenDates(DateTime begin, DateTime end)
     
     }
 }
